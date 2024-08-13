2e3fcef298afae9791fbc328967a60d960815be8
Huzaifa-bi <121827971+Huzaifa-bi@users.noreply.github.com>
2024-08-13 15:11:17 +0500

    final commit
    All task completed

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public InputField inputField;
+    public string userName;
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    private void Update()
+    {
+        if (inputField != null)
+        {
+            userName = inputField.text;
+        }
+    }
+}
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -12,20 +12,28 @@ public class MainManager : MonoBehaviour
 
     public Text ScoreText;
     public GameObject GameOverText;
-    
+    public Text nameText;
+    public Text highestScoreText;
+
     private bool m_Started = false;
     private int m_Points;
-    
+    private int currentScore;
+    private string playerName;
+
     private bool m_GameOver = false;
 
-    
-    // Start is called before the first frame update
+    private void Awake()
+    {
+        SetPlayerName(GameManager.Instance.userName);
+        LoadPlayerData();
+    }
+
     void Start()
     {
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -70,7 +78,38 @@ public class MainManager : MonoBehaviour
 
     public void GameOver()
     {
+        UpdateScore(m_Points);
         m_GameOver = true;
         GameOverText.SetActive(true);
+    }
+
+    public void SetPlayerName(string name)
+    {
+        playerName = name;
+        nameText.text = "Name: " + playerName;
+        LoadPlayerData();
+    }
+
+    private void LoadPlayerData()
+    {
+        int highScore = PlayerPrefs.GetInt(playerName + "_highScore", 0);
+        highestScoreText.text = "Best Score: " + highScore.ToString();
+    }
+
+    public void UpdateScore(int score)
+    {
+        currentScore = score;
+        int highScore = PlayerPrefs.GetInt(playerName + "_highScore", 0);
+
+        if (currentScore > highScore)
+        {
+            PlayerPrefs.SetInt(playerName + "_highScore", currentScore);
+            PlayerPrefs.Save();
+            highestScoreText.text = "Best Score: " + currentScore.ToString();
+        }
+    }
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
